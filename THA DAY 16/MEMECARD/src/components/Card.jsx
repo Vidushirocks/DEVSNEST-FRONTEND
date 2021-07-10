4b3496ee2469139e6eb787791fa74b2d7ebd0d3e
Vidushirocks <66163449+Vidushirocks@users.noreply.github.com>
2021-07-10 12:36:56 +0530

    Add files via upload

---
+++ THA DAY 16/MEMECARD/src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = () => {
+    return(
+        <div className="Card">
+            <img src= "https://humornama.com/wp-content/uploads/2020/09/engineer-viva-meme-on-tmkoc.jpg" alt= "bbt meme" />
+            <h2>React card</h2>
+        </div>
+    );
+}
+export default Card;
