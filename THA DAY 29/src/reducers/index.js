a2613347955d48cd62276425cfaa0b493f0ef7ed
Vidushirocks <66163449+Vidushirocks@users.noreply.github.com>
2021-09-04 17:54:57 +0530

    Add files via upload

--- THA DAY 29/src/reducers/index.js
+++ THA DAY 29/src/reducers/index.js
@@ -1,1 +1,11 @@
+import { combineReducers } from "redux";
+import PlaceReducer from './placeReducer';
+import PlaceDataReducer from './placeDatareducer';
+import themeReducer from './themeReducer';
 
+const rootReducer = combineReducers({
+    place: PlaceReducer,
+    placeData: PlaceDataReducer,
+    theme: themeReducer,
+});
+export default rootReducer;
