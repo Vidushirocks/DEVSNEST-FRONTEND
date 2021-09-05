d870a84859fe499e86f0006e7c597c14325041d9
Vidushirocks <66163449+Vidushirocks@users.noreply.github.com>
2021-09-05 20:06:44 +0530

    Add files via upload

--- THA DAY 30-31/src/condi/app.js
+++ THA DAY 30-31/src/condi/app.js
@@ -1,141 +1,141 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "./cour.css";
-import Row from './row';
+import Row from './row'
 import Card from './card';
 
-const Android  = () => {
+const Language = () => {
     const cards = [
         {
             id: Math.random()*10,
-            src:"https://courses.javacodegeeks.com/wp-content/uploads/2020/02/1625800_2ffc_5-300x300.jpg",
-            title: "Android Java Masterclass",
-            desc: "Improve your career options by learning Android app Development. Master Android Studio and build your first app.",
-            price: "₹385",
-            cutprice:"₹1,400",
-            rating: "★★★★★",
-            view: "https://www.udemy.com/course/master-android-7-nougat-java-app-development-step-by-step/"
-            
-        },
-        {
-            id: Math.random()*10,
-            src:"https://www.slashgear.com/wp-content/uploads/2011/01/android_s2-hexcode_pre.jpg",
-            title: "The Complete Android 10",
-            desc: " Learn Android Development From Beginner to Advanced Developer. Build Apps like Trello and 7Min Workout.",
-            price: "₹499",
+            src:"https://coursemarks.com/wp-content/uploads/2020/11/1581488_e3e1_2.jpg",
+            title: "C# Masterclass",
+            desc: "Learn C# Programming - WPF, Databases, Linq, Collections, Game Development with Unity. More than just the C# basics!",
+            price: "₹550",
             cutprice:"₹1,500",
-            rating: "★★★★☆",
-            view: "https://www.udemy.com/course/android-kotlin-developer/"
-            
-        },
-        {
-            id: Math.random()*10,
-            src:"https://cdn.eduonix.com/assets/images/header_img/2020062508133511994.jpg",
-            title: "Android Multithreading Class",
-            desc: "Complete guide to multithreading in Android. From fundamentals to Thread Pools, RxJava and Kotlin.",
-            price: "₹786",
-            cutprice:"₹1,999",
             rating: "★★★★★",
-            view: "https://www.udemy.com/course/android-multithreading/"
+            view: "https://www.udemy.com/course/complete-csharp-masterclass/"
             
         },
         {
             id: Math.random()*10,
-            src:"https://media.onlinefreecourse.net/udemy/2018/08/26061733/cPNPjQ.jpg",
-            title: "The Mobile Development",
-            desc: "In this course, you'll learn Android Development and get to build your own Android R apps by using Java..",
-            price: "₹450",
-            cutprice:"₹1,599",
-            rating: "★★★☆☆",
-            view: "https://www.udemy.com/course/java-android-complete-guide/"
-            
-        },
-        {
-            id: Math.random()*10,
-            src:"https://digitalmediaglobe.com/wp-content/uploads/2017/04/Udemy-Android-Course-Review.jpg",
-            title: "Android App Development",
-            desc: " Android App Development for complete beginners, make beautiful apps, get a job , Java AND Kotlin!",
-            price: "₹799",
-            cutprice:"₹2,999",
-            rating: "★★★★★",
-            view: "https://www.udemy.com/course/the-complete-android-developer-bootcamp/"
-            
-        },
-        {
-            id: Math.random()*10,
-            src:"https://www.educative.io/v2api/collection/6100126873419776/6055343571337216/image/4657441726791680",
-            title: "The Comprehensive 2021",
-            desc: "Master Modern Android API's: ROOM Database, ML Kit Face Recognition, FirebaseMaps and Android Studio",
-            price: "₹459",
+            src:"https://www.edx.org/static/6a7fb061164e43e112067bfaaceefaa9/learn_python.jpg",
+            title: "Code with Python",
+            desc: " The complete Python bootcamp for 2021. Learn Python 3 from beginner to expert. Build complete Python applications.",
+            price: "₹499",
             cutprice:"₹1,499",
             rating: "★★★★☆",
-            view: "https://www.udemy.com/course/android-development-java-android-studio-masterclass/"
+            view: "https://www.udemy.com/course/learn-to-code-with-python/"
             
         },
         {
             id: Math.random()*10,
-            src:"https://www.spaceotechnologies.com/wp-content/uploads/2020/12/android_app_development_course_for_beginners1.png",
-            title: "The Complete Android Kotlin",
-            desc: "Build online games, and apps for Android Q, like Pokémon , twitter,Tic Tac Toe, and notepad using Kotlin",
-            price: "500",
-            cutprice:"₹2,000",
+            src:"https://www.takethiscourse.net/wp-content/uploads/2020/02/Complete-Python-Masterclass.jpg",
+            title: "Complete Python",
+            desc: "Direct Class Room Videos To give Left & Right Anywhere about Python 3 Concepts In Detail From Beginner to Expert Level",
+            price: "₹786",
+            cutprice:"₹2,500",
             rating: "★★★★★",
-            view: "https://www.udemy.com/course/the-complete-kotlin-developer-course/"
+            view: "https://www.udemy.com/course/learn-complete-python-tutorial-in-simple-way/"
             
         },
         {
             id: Math.random()*10,
-            src:"https://udemydownload.com/wp-content/uploads/2021/04/Flutter-Firebase-Build-a-Complete-App-for-iOS-Android-390x220.jpg",
-            title: "Android App Development",
-            desc: " Learn Kotlin Android App Development And Become an Android Developer. Incl. Kotlin Tutorial and Android Tutorial.",
-            price: "₹899",
-            cutprice:"₹3,120",
-            rating: "★★★★☆",
-            view: "https://www.udemy.com/course/android-oreo-kotlin-app-masterclass/"
-            
-        },
-        {
-            id: Math.random()*10,
-            src:"https://miro.medium.com/max/966/1*ZQJ7-0G48FRy72VJ77hrfA.png",
-            title: "The Android 11 Jetpack",
-            desc: "Android Development with Jetpack, MVVM Clean Architecture, Compose, UnitTesting, Dagger/Hilt, Retrofit.",
-            price: "₹385",
+            src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTssGrDdAVm4jfdX459s-xuT_XOLm2k7J3ybg&usqp=CAU",
+            title: "Asynchronous JavaScript",
+            desc: "Become an expert in Asynchronous JavaScript, Callbacks, Promises, Async/Await.",
+            price: "₹450",
             cutprice:"₹1,800",
             rating: "★★★☆☆",
-            view: "https://www.udemy.com/course/android-architecture-componentsmvvm-with-dagger-retrofit/"
-            
-        },
-        {
-            id: Math.random()*10,
-            src:"https://i0.wp.com/freecoursewebsite.com/wp-content/uploads/2020/09/Download-Android-App-Development-Masterclass-using-Kotlin-2020.jpg?fit=521%2C305&ssl=1&resize=350%2C200",
-            title: "Kotlin for Android Masterclass",
-            desc: "Learn Kotlin Coroutines in depth and practice advanced Coroutines use cases in Android application",
-            price: "₹880",
-            cutprice:"₹2,000",
-            rating: "★★★★★",
-            view: "https://www.udemy.com/course/kotlin-coroutines-in-android/"
-            
-        },
-        {
-            id: Math.random()*10,
-            src:"https://freecourseudemy.com/wp-content/uploads/2020/01/The-Complete-Android-Kotlin-Developer-Course-1.jpg",
-            title: "Android Architecture Masterclass",
-            desc: " Embrace clean design and architecture in Android apps. Employ Dependency Injection of Concerns principle",
-            price: "₹400",
-            cutprice:"₹1,280",
-            rating: "★★★☆☆",
-            view: "https://www.udemy.com/course/android-architecture/"
-            
-        },
-        {
-            id: Math.random()*10,
-            src:"http://www.cromacampus.com/wp-content/uploads/2017/08/android-training-croma-campus.jpg",
-            title: "Make a Freaking Android App",
-            desc: "Make apps for Android M - Marshmallow, using Android Studio for Mac or Windows.",
-            price: "₹567",
-            cutprice:"₹1,900",
-            rating: "★★★★☆",
             view: "https://www.udemy.com/course/how-to-make-a-freaking-android-app/"
+            
+        },
+        {
+            id: Math.random()*10,
+            src:"https://helpdeskgeek.com/wp-content/pictures/2020/01/C.png",
+            title: "Fun With C++",
+            desc: " Take this course to learn C++ which you can use for ethical hacking, game development, & software you use daily!",
+            price: "₹799",
+            cutprice:"₹2,458,",
+            rating: "★★★★★",
+            view: "https://www.udemy.com/course/react-js-mastering-redux/https://www.udemy.com/course/learn-c-plus-plus-from-beginner-to-advanced/"
+            
+        },
+        {
+            id: Math.random()*10,
+            src:"https://i.ytimg.com/vi/0zuolvgpAaY/maxresdefault.jpg",
+            title: "Advanced C Programming",
+            desc: "Become a True Master of C Programming Language - Confidently Apply for Real Time or Embedded C Jobs!",
+            price: "₹459",
+            cutprice:"₹2,500",
+            rating: "★★★★☆",
+            view: "https://www.udemy.com/course/advanced-c-programming-course/"
+            
+        },
+        {
+            id: Math.random()*10,
+            src:"https://summarynetworks.com/wp-content/uploads/2021/07/1627063788_maxresdefault.jpg",
+            title: "C Programming For All",
+            desc: "Learn C programming for beginners, Learn programming fundamentals, Be able to create C applications",
+            price: "500",
+            cutprice:"₹2,399",
+            rating: "★★★★★",
+            view: "https://www.udemy.com/course/learn-c-programming-for-complete-beginners/"
+            
+        },
+        {
+            id: Math.random()*10,
+            src:"https://i.ytimg.com/vi/SqrbIlUwR0U/maxresdefault.jpg",
+            title: "Go Syntax Explained (2)",
+            desc: " Arrays and Slices - study, review, practice: a learning method for those needing to revisit periodically or drill Golang",
+            price: "₹899",
+            cutprice:"₹3,599",
+            rating: "★★★★☆",
+            view: "https://www.udemy.com/course/golang-arrays-slices-tutorial/"
+            
+        },
+        {
+            id: Math.random()*10,
+            src:"https://coursemarks.com/wp-content/uploads/2020/11/1901724_6657_18.jpg",
+            title: "Learning Path: Master...",
+            desc: " Learn how to use this incredible language to build web-scale, real-time systems and applications on your own",
+            price: "₹385",
+            cutprice:"₹1,100",
+            rating: "★★★☆☆",
+            view: "https://www.udemy.com/course/learning-path-master-googles-go/"
+            
+        },
+        {
+            id: Math.random()*10,
+            src:"https://dl3.tut4dev.ir/files/2781/ProBanner/banner.png",
+            title: "Java for Absolute...",
+            desc: " A course on Java for complete beginners to computer programming, for those who want concepts explained.",
+            price: "₹880",
+            cutprice:"₹2,999",
+            rating: "★★★★★",
+            view: "https://www.udemy.com/course/java-for-absolute-beginners/"
+            
+        },
+        {
+            id: Math.random()*10,
+            src:"https://i.ytimg.com/vi/XaU8JKQW0Ao/maxresdefault.jpg",
+            title: "Develop Minecraft Plugins",
+            desc: "  High-quality tutorials providing you with the most sustainable practices available to make Minecraft plugins.",
+            price: "₹400",
+            cutprice:"₹1,200",
+            rating: "★★★☆☆",
+            view: "https://www.udemy.com/course/develop-minecraft-plugins-java-programming/"
+            
+        },
+        {
+            id: Math.random()*10,
+            src:"https://thecodelearners.com/wp-content/uploads/2020/05/Javascript-Advance-Object-Operations.jpg",
+            title: "Advanced Javascript",
+            desc: "In only seven hours you will learn enough javascript to transform from a Junior JS Dev into a Senior JS Guru",
+            price: "₹567",
+            cutprice:"₹2,100",
+            rating: "★★★★☆",
+            view: "https://www.udemy.com/course/javascript-advanced/"
             
         },
         
@@ -177,8 +177,8 @@ const Android  = () => {
   
   return (
       <div className="App">
-         <h1 class="heading">
-        <span>Courses On Andriod Development</span>
+          <h1 class="heading">
+        <span>Courses On Programming Languages</span>
       </h1>
       <div id="cards_landscape_wrap-2">
         <div className="container">
@@ -188,4 +188,4 @@ const Android  = () => {
     </div>
   );
 };
-export default Android;
+export default Language;
