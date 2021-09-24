a6df0813163adefcd708125d10e6bdf3fe9fb519
Vidushirocks <66163449+Vidushirocks@users.noreply.github.com>
2021-09-24 19:06:32 +0530

    Add files via upload

---
+++ THA DAY 35/src/data/users.js
@@ -0,0 +1,104 @@
+const users = [
+    {
+      id: 1,
+      name: "Virat Kohli",
+      username: "virat.kohli",
+    },
+    {
+      id: 2,
+      name: "Kane Williamson",
+      username: "kane_s_w",
+    },
+    {
+      id: 3,
+      name: "AB de Villers",
+      username: "abdevillers17",
+    },
+    {
+      id: 4,
+      name: "Vicky Kaushal",
+      username: "vickykaushal09",
+    },
+    {
+      id: 5,
+      name: "이민호 leeminho",
+      username: "actorleeminho",
+    },
+    {
+      id: 6,
+      name: "김범 kimbum",
+      username: "k.kbeom",
+    },
+    {
+      id: 7,
+      name: "황인엽 Hwang In Youp",
+      username: "hi_high_hiy",
+    },
+    {
+      id: 8,
+      name: "Matt Le Blanc",
+      username: "mleblanc",
+    },
+    {
+      id: 9,
+      name: "Hrithik Roshan",
+      username: "hrithikroshan",
+    },
+    {
+      id: 10,
+      name: "Jessica Ashley",
+      username: "jessash",
+    },
+    {
+      id: 11,
+      name: "Katia Tara",
+      username: "donald.duck",
+    },
+    {
+      id: 12,
+      name: "Adam Savage",
+      username: "adam.is.savage",
+    },
+    {
+      id: 13,
+      name: "Daniel Redhill",
+      username: "harry.trotter",
+    },
+    {
+      id: 14,
+      name: "Indiana Jones",
+      username: "Indy Shot First",
+    },
+    {
+      id: 15,
+      name: "Claudia Beckam",
+      username: "handball2020",
+    },
+    {
+      id: 16,
+      name: "Fabio Edwards",
+      username: "EastCoastLion",
+    },
+    {
+      id: 17,
+      name: "Jonah Roads",
+      username: "AlwaysTheFunnyGuy",
+    },
+    {
+      id: 18,
+      name: "Damian Knight",
+      username: "The Rook",
+    },
+    {
+      id: 19,
+      name: "Rowan Atkinson",
+      username: "Mr Bean",
+    },
+    {
+      id: 20,
+      name: "Cristiano Ronaldo",
+      username: "cristiano",
+    },
+  ];
+  
+  export default users;
